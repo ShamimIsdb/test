7a01dda393436496c7518d273d818818185d0d79
Shamim Ahmed <141951573+ShamimIsdb@users.noreply.github.com>
2024-01-27 20:48:29 +0600

    uploads

---
+++ ResumeManagement/ResumeManagementSLN/ResumeManagementAPI/Migrations/20230728165624_2nd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2nd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Usename",
+                table: "TokenInfo",
+                newName: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "TokenInfo",
+                newName: "Usename");
+        }
+    }
+}
